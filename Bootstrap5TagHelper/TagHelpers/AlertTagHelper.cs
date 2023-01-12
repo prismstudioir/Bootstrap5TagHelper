1008780552e1c5ff438b548d5c7506b95c138cbe
Alireza Rezaie (majnoon) <majnoonrze@outlook.com>
2023-01-12 20:41:23 +0330

    - Add Button TagHelper

--- Bootstrap5TagHelper/TagHelpers/AlertTagHelper.cs
+++ Bootstrap5TagHelper/TagHelpers/AlertTagHelper.cs
@@ -2,7 +2,7 @@
 
 namespace Bootstrap5TagHelper.TagHelpers
 {
-
+    [HtmlTargetElement("BAlert")]
     public class AlertTagHelper : TagHelper
     {
         public string Message { get; set; }
@@ -17,15 +17,16 @@ namespace Bootstrap5TagHelper.TagHelpers
             Light=6,
             Dark=7
         }
-
+        public string ExtraClass { get; set; }
+        public string OtherStyle { get; set; }
         public AlertType Type { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "div";
             output.TagMode = TagMode.StartTagAndEndTag;
-            output.Attributes.Add("class", "alert alert-" + Type.ToString().ToLower());
+            output.Attributes.Add("class", "alert alert-" + Type.ToString().ToLower() + " " + ExtraClass);
             output.Attributes.Add("role", "alert");
-
+            output.Attributes.Add("Style", OtherStyle);
             output.Content.SetContent(Message);
         }
     }
---
+++ Bootstrap5TagHelper/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap5TagHelper.TagHelpers
+{
+    [HtmlTargetElement("BButton")]
+    public class ButtonTagHelper : TagHelper
+    {
+        public string Text { get; set; }
+        public enum ButtonType
+        {
+            Primary = 0,
+            Secondary = 1,
+            Success = 2,
+            Danger = 3,
+            Warning = 4,
+            Info = 5,
+            Light = 6,
+            Dark = 7,
+            Outline_Primary = 8,
+            Outline_Secondary = 9,
+            Outline_Success = 10,
+            Outline_Danger = 11,
+            Outline_Warning = 12,
+            Outline_Info = 13,
+            Outline_Light = 14,
+            Outline_Dark = 15
+
+
+            
+        }
+        public string ExtraClass { get; set; }
+        public string OtherStyle { get; set; }
+
+        public ButtonType Type { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button ";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Add("class", "btn btn-" + Type.ToString().ToLower().Replace("_","-")+" "+ ExtraClass);
+            output.Attributes.Add("Style", OtherStyle);
+            output.Content.SetContent(Text);
+        }
+    }
+}
