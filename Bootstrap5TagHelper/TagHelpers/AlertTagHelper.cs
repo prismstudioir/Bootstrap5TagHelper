bc07dad35d999744b2dad26a7ea6b5ef56190e16
Alireza Rezaie (majnoon) <majnoonrze@outlook.com>
2023-01-12 20:18:24 +0330

    - Add Alert TagHelper

---
+++ Bootstrap5TagHelper/TagHelpers/AlertTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Bootstrap5TagHelper.TagHelpers
+{
+
+    public class AlertTagHelper : TagHelper
+    {
+        public string Message { get; set; }
+        public enum AlertType
+        {
+            Primary = 0,
+            Secondary = 1,
+            Success = 2,
+            Danger=3,
+            Warning=4,
+            Info=5,
+            Light=6,
+            Dark=7
+        }
+
+        public AlertType Type { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Add("class", "alert alert-" + Type.ToString().ToLower());
+            output.Attributes.Add("role", "alert");
+
+            output.Content.SetContent(Message);
+        }
+    }
+}
